183441be67ab5cb95717a187c998480b455fc8a9
Jessica <jessica12492@gmail.com>
2023-07-17 10:42:12 -0500

    add font to assets

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
-  factory :user do
+  factory :user do |f|
+    f.email{faker::Internet.email}
+    f.password{faker::Internet.password(min_length: 10,max_length: 20, special_characters: true)}
     
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(email: "sabrina@gmail.com", password:"123456"  )}
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  
 end
